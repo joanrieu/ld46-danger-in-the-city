9790b55fd84750aac1c63c4265ca432605c140b9
Joan Rieu <joan@joanrieu.com>
2020-04-18 20:38:12 -0400

    rescale geometry to real world dimensions

--- Car.gd
+++ Car.gd
@@ -1,8 +1,8 @@
-extends VehicleBody
+extends Spatial
 
-func _physics_process(delta):
-	self.engine_force = 3000 if Input.is_action_pressed("car_accelerate") else 0
-	self.brake = 30 if Input.is_action_pressed("car_brake") else 0
-	var left = .5 if Input.is_action_pressed("car_left") else 0
-	var right = -.5 if Input.is_action_pressed("car_right") else 0
-	self.steering = left + right
+#func _physics_process(delta):
+#	self.engine_force = 3000 if Input.is_action_pressed("car_accelerate") else 0
+#	self.brake = 30 if Input.is_action_pressed("car_brake") else 0
+#	var left = .5 if Input.is_action_pressed("car_left") else 0
+#	var right = -.5 if Input.is_action_pressed("car_right") else 0
+#	self.steering = left + right
