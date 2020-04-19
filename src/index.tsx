0d9a2692920849b4929da4f9c31e1d6f13a875f0
Joan Rieu <joan@joanrieu.com>
2020-04-19 11:03:44 -0400

    add car controls and follower camera

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,37 @@ import React, { Fragment, Suspense, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Canvas, useFrame, useLoader, useThree } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import carUrl from "./car.glb";
+import carGltfUrl from "./car.glb";
+import { Vector3 } from "three";
+
+const keys: Record<string, true> = {};
+document.addEventListener("keydown", (event) => (keys[event.key] = true));
+document.addEventListener("keyup", (event) => delete keys[event.key]);
 
 function Car() {
-  const gltf = useLoader(GLTFLoader, carUrl);
+  const { camera } = useThree();
+  const gltf = useLoader(GLTFLoader, carGltfUrl);
+  const car = gltf.scene;
+
   useFrame(() => {
-    gltf.scene.rotateY(Math.PI / 100);
+    car.translateZ(-1);
+    if (keys.ArrowLeft) car.rotateY(Math.PI / 100);
+    if (keys.ArrowRight) car.rotateY(-Math.PI / 100);
+
+    const coef = 0.7;
+    camera.position
+      .multiplyScalar(coef)
+      .addScaledVector(
+        new Vector3(0, 2, 5).applyQuaternion(car.quaternion).add(car.position),
+        1 - coef
+      );
+    camera.lookAt(car.position.clone().add(new Vector3(0, 1, 0)));
   });
-  return <primitive object={gltf.scene} dispose={null} />;
+
+  return <primitive object={car} dispose={null} />;
 }
 
 function Game() {
-  const { camera } = useThree();
-  useEffect(() => {
-    camera.position.y = 1;
-  }, [camera]);
   return (
     <>
       <ambientLight intensity={0.5} />
